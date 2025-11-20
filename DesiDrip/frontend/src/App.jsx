727fc1a0be18e6a74d6d5dda180e9b9967211379
Bharat Pandav <bharatpandav137@gmail.com>
2025-11-20 02:04:58 -0800

    App.jsx

--- DesiDrip/frontend/src/App.jsx
+++ DesiDrip/frontend/src/App.jsx
@@ -12,7 +12,6 @@ import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
 import Verify from "./pages/Verify";
-// import AdminLogin from "./pages/AdminLogin";
 
 // for notification prodtuct added or no to cart library header
 import { ToastContainer } from 'react-toastify';
@@ -33,7 +32,6 @@ const App = () => {
         <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/admin" element={<AdminLogin />} /> */}
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/verify" element={<Verify />} />
