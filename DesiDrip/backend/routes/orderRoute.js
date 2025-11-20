574b0f6483c813d929dc322432def2b392e9d68f
Bharat Pandav <bharatpandav137@gmail.com>
2025-11-20 01:40:32 -0800

    orderRoute.js

--- DesiDrip/backend/routes/orderRoute.js
+++ DesiDrip/backend/routes/orderRoute.js
@@ -2,29 +2,17 @@ import express from 'express';
 import { allOrders, userOrders,placeOrder, updateStatus,} from '../controllers/orderController.js';
 import adminAuth from '../middleware/adminAuth.js';
 import authUser from '../middleware/auth.js';
-// , placeOrderStripe, placeOrderRazorpay, verifyStripe,verifyRazorpay
-
 
 const orderRouter = express.Router();
-
 
 // admin features
 orderRouter.post('/list', adminAuth,allOrders);
 orderRouter.post('/status', adminAuth, updateStatus);
 
-
 // payment features
 orderRouter.post('/place', authUser,placeOrder);
-// orderRouter.post('/stripe', authUser, placeOrderStripe);
-// orderRouter.post('/razorpay', authUser, placeOrderRazorpay);
-
 
 // user features
 orderRouter.post('/userorders', authUser, userOrders);
-
-
-// verify payment
-// orderRouter.post('/verifyStripe', authUser, verifyStripe);
-// orderRouter.post('/verifyRazorpay', authUser, verifyRazorpay);
 
 export default orderRouter;
